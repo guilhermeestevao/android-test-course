aa2dc70d7478350bb1744afa43453cf2d9886375
guilherme <guilhermeestevo@gmail.com>
2024-02-25 23:08:02 -0300

    Getting started with testing

---
+++ core/domain/src/test/kotlin/com/ivy/core/domain/algorithm/calc/RawStatsTest.kt
@@ -0,0 +1,61 @@
+package com.ivy.core.domain.algorithm.calc
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import com.ivy.core.domain.algorithm.calc.data.RawStats
+import com.ivy.core.persistence.algorithm.calc.CalcTrn
+import com.ivy.data.CurrencyCode
+import com.ivy.data.transaction.TransactionType
+import org.junit.jupiter.api.Test
+import java.time.Instant
+
+internal class RawStatsTest {
+
+    @Test
+    fun `Test creating raw stats from transactions`() {
+
+        val tenSecondsAgo = Instant.now().minusSeconds(10)
+        val fiveSecondsAgo = Instant.now().minusSeconds(5)
+        val threeSecondsAgo = Instant.now().minusSeconds(3)
+
+        val terns = listOf<CalcTrn>(
+            CalcTrn(
+                amount = 1.0,
+                currency = "EUR",
+                type = TransactionType.Income,
+                time = tenSecondsAgo
+            ),
+            CalcTrn(
+                amount = 2.0,
+                currency = "USD",
+                type = TransactionType.Income,
+                time = fiveSecondsAgo
+            ),
+            CalcTrn(
+                amount = -3.0,
+                currency = "BRL",
+                type = TransactionType.Expense,
+                time = threeSecondsAgo
+            )
+        )
+
+        val expected = RawStats(
+            incomes = mapOf(
+                "EUR" to 1.0,
+                "USD" to 2.0
+            ),
+            expenses = mapOf(
+                "BRL" to -3.0
+            ),
+            incomesCount = 2,
+            expensesCount = 1,
+            newestTrnTime = threeSecondsAgo
+        )
+
+        val actual = rawStats(terns)
+
+        assertThat(actual).isEqualTo(expected)
+        
+    }
+
+}
